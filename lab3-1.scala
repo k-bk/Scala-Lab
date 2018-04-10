50c9aeef9be13ab625cb3522228dfb24e0d30e80
karolBak <bak.karolek@gmail.com>
2018-04-10 14:35:28 +0200

    Finished 3.1

--- lab3-1.scala
+++ lab3-1.scala
@@ -1,11 +1,23 @@
 object Appl {
-  def main(args: Array[String] = {
+  def readFile(fileName: String) = try {
     println("Opening the file...")
-    val inFile = scala.io.Source.fromFile("logins.txt")
-    for (line <- inFile.getLines) println(line)
-    println("Closing the file...")
-    inFile.close
+    val inFile = scala.io.Source.fromFile(fileName)
+    try {
+      for (line <- inFile.getLines) println(line)
+      val x = 100 / inFile.getLines.length
+    } finally {
+      println("Closing the file...")
+      inFile.close
+    }
+  } catch {
+    case ex: java.io.FileNotFoundException => println(ex.getMessage)
+    case ex: Throwable => println("Default exception handler: " + ex.getMessage)
+  }
+
+  def main(args: Array[String]) {
+    readFile("logins.txt")
   }
 }
+
 
 
